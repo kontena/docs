cb228867c0eccefa7fcb90cf7f044af02da4d433
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-09-01 13:31:34 +0300

    use correct env

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ue
 
-docker login -u kontenabot -p $DOCKER_HUB_PASSWORD
+docker login -u kontenabot -p $DOCKERHUB_PASSWORD
 docker build --pull -t kontena/docs:latest .
 docker push kontena/docs:latest
