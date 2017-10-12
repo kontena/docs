d6928f35cbc7dfb7e4601e38fa7c33a54d62b00d
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-10-12 17:53:43 +0300

    fix click tracking (#36)

--- _scripts/tracks.js
+++ _scripts/tracks.js
@@ -1,5 +1,5 @@
 require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function() {
+    gitbook.events.bind("start", function(e, config) {
         var waitForEl = function(selector, callback) {
             if (jQuery(selector).length) {
                 callback();
@@ -12,6 +12,7 @@ require(["gitbook"], function(gitbook) {
         waitForEl('#launcher', function() {
             var iframe = $("#launcher").contents();
             $(iframe).find("#Embed").click(function(event) {
+                console.log("support chat opened")
                 mixpanel.track("support-chat.opened", {});
             })
         });
